8608963a5d9f6c92fd6368ffa4fc0b2ae0633606
Jandson <jandson.macedo2301@gmail.com>
2022-01-31 11:34:13 -0300

    first commit

---
+++ src/main/java/com/github/jand/online/novel/api/OnlineNovelApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.jand.online.novel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineNovelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineNovelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jand/online/novel/api/OnlineNovelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jand.online.novel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineNovelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
